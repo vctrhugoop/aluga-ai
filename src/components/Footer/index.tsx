53cf88d451139353eedfeb0fb054dcb2df62e7f6
vctrhugoop <vhdestroy@hotmail.com>
2024-03-12 20:31:59 -0300

    ✨ feat: changes footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,15 @@
 export function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full bg-teal-600 p-4 text-center text-zinc-50">
+    <footer className="absolute bottom-0 w-full bg-teal-600 p-4 text-center text-xs text-zinc-50">
+      <p>&copy; 2024 AlugaAí | Todos os direitos reservados.</p>
+      <div className="mx-auto my-3 h-[1px] max-w-6xl bg-zinc-50"></div>
       <p>
-        &copy; 2024 AlugaAí - Desenvolvido por{" "}
-        <a href="https://github.com/vctrhugoop" className="font-bold">
+        Desenvolvido por{" "}
+        <a
+          href="https://github.com/vctrhugoop"
+          target="_blank"
+          className="font-bold"
+        >
           Victor Oliveira
         </a>
       </p>
