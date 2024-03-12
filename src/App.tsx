ed425960690b02f05c8cc6aa563f50dc579059e3
vctrhugoop <vhdestroy@hotmail.com>
2024-03-11 21:03:52 -0300

    ✨ feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import { Header } from "./components/Header";
 
+import HeroIllustration from "./assets/hero-illustration.svg";
+
 export function App() {
   return (
-    <div className="min-h-screen">
+    <div className="mx-auto min-h-screen max-w-6xl">
       <Header />
+      <section className="mx-auto my-9 flex max-w-6xl items-center justify-between px-4 py-6 lg:px-0">
+        <div className="space-y-2">
+          <h1 className="text-2xl font-bold text-zinc-900 lg:text-4xl">
+            Desbrave o Caminho da Liberdade
+          </h1>
+          <p className="text-zinc-600 lg:text-xl">
+            Descubra a experiência única de locação de carros feita sob medida
+            para você.
+          </p>
+        </div>
+        <div className="hidden min-w-[500px] md:block">
+          <img src={HeroIllustration} />
+        </div>
+      </section>
     </div>
   );
 }
