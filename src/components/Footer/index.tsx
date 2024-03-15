5514e3ca1b1a2e36088b23c9bb0741b531d3f02a
vctrhugoop <vhdestroy@hotmail.com>
2024-03-15 20:46:04 -0300

    ✨ feat: remove color bg and divisor

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,7 @@
 export function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full bg-teal-600 p-4 text-center text-xs text-zinc-50">
+    <footer className="absolute bottom-0 w-full space-y-2 border-t p-4 text-center text-xs text-zinc-50">
       <p>&copy; 2024 AlugaAí | Todos os direitos reservados.</p>
-      <div className="mx-auto my-3 h-[1px] max-w-6xl bg-zinc-50"></div>
       <p>
         Desenvolvido por{" "}
         <a
