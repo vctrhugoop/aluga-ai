479fc76ce2906a8981e92246935a567387c1035c
vctrhugoop <vhdestroy@hotmail.com>
2024-03-15 22:29:10 -0300

    ✨ feat: create date picker component

---
+++ src/components/DatePicker/index.tsx
@@ -0,0 +1,48 @@
+import { CalendarIcon } from "@radix-ui/react-icons";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+
+export function DatePicker() {
+  const [date, setDate] = React.useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? (
+            format(date, "dd 'de' MMMM 'de' yyyy", { locale: ptBR })
+          ) : (
+            <span>Selecione uma data</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          locale={ptBR}
+          fromDate={new Date()}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
