d3e9ef6815d0e41e76ec0f76c466226b0c416057
vctrhugoop <vhdestroy@hotmail.com>
2024-03-19 18:31:31 -0300

    ✨ feat: add navitage router new page

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Home } from "./pages/Home";
+import { MyBookings } from "./pages/MyBookings";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
       </Route>
     </Routes>
   );
