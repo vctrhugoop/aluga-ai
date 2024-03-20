e6ec2db11ffafaa1025607491f1c6155e2a2f969
vctrhugoop <vhdestroy@hotmail.com>
2024-03-20 07:33:32 -0300

    🎉 init: seteup page my bookings

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="absolute bottom-0 w-full space-y-2 border-t p-4 text-center text-xs text-zinc-50">
+    <footer className="absolute bottom-0 right-0 w-full space-y-2 border-t p-4 text-center text-xs text-zinc-50">
       <p>&copy; 2024 AlugaAí | Todos os direitos reservados.</p>
       <p>
         Desenvolvido por{" "}
--- src/pages/MyBookings.tsx
+++ src/pages/MyBookings.tsx
@@ -1,7 +1,113 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { useBookingCar } from "@/hooks/useBooking";
+import { Calendar, Car, CurrencyDollar, User } from "@phosphor-icons/react";
+import { format } from "date-fns";
 
 export function MyBookings() {
   const { bookingsCars } = useBookingCar();
 
-  return <div>{bookingsCars.length >= 0 ? <>oi</> : <>oi</>}</div>;
+  return (
+    <div className="mx-auto max-w-6xl ">
+      {bookingsCars.length <= 0 ? (
+        <div>
+          <p>Não há reservas de carros no momento.</p>
+          {/*adicionar um ícone ou imagem aqui*/}
+        </div>
+      ) : (
+        <section className="mx-auto max-w-6xl space-y-4 px-4 pb-48 pt-6">
+          <h1>Minhas Reservas</h1>
+          {bookingsCars.map((bookingCar) => (
+            <Card key={bookingCar.car.id} className="p-6">
+              <CardHeader className="space-y-0 text-center">
+                <CardTitle>Código da Reserva</CardTitle>
+                <CardDescription>
+                  {bookingCar.car.id.substring(0, 8).toUpperCase()}
+                </CardDescription>
+              </CardHeader>
+              <Separator className="my-4" />
+              <CardContent className="space-y-4">
+                <div className="space-y-2">
+                  <h3 className="flex font-semibold leading-none tracking-tight ">
+                    <Car />
+                    Veículo
+                  </h3>
+                  <div>
+                    <p className="text-sm text-muted-foreground">
+                      Modelo: {bookingCar.car.name}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      Categótia: {bookingCar.car.category}
+                    </p>
+                  </div>
+                  <img
+                    src={bookingCar.car.imageURL}
+                    className="hidden w-48 lg:block"
+                  />
+                </div>
+                <div className="space-y-2">
+                  <h3 className="flex font-semibold leading-none tracking-tight">
+                    <User />
+                    Dados do condutor
+                  </h3>
+                  <div>
+                    <p className="text-sm text-muted-foreground">
+                      Nome: {bookingCar.formData.firstName}{" "}
+                      {bookingCar.formData.lastName}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      E-mail: {bookingCar.formData.email}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex justify-between">
+                  <div className="space-y-2">
+                    <h3 className="flex font-semibold leading-none tracking-tight">
+                      <Calendar />
+                      Data de retirada
+                    </h3>
+                    <p className="text-sm text-muted-foreground">
+                      {bookingCar.formData.date.from && (
+                        <p>
+                          {format(bookingCar.formData.date.from, "dd/MM/yyyy")}
+                        </p>
+                      )}
+                    </p>
+                  </div>
+                  <div className="space-y-2">
+                    <h3 className="flex font-semibold leading-none tracking-tight">
+                      <Calendar />
+                      Data de devolução
+                    </h3>
+                    <p className="text-sm text-muted-foreground">
+                      {bookingCar.formData.date.to && (
+                        <p>
+                          {format(bookingCar.formData.date.to, "dd/MM/yyyy")}
+                        </p>
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <h3 className="flex font-semibold leading-none tracking-tight">
+                    <CurrencyDollar />
+                    Valor
+                  </h3>
+                  <p className="text-sm text-muted-foreground">
+                    {bookingCar.car.price}
+                  </p>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </section>
+      )}
+    </div>
+  );
 }
