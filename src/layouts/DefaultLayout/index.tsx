5633f41d7c22c622611a2b97264ae8d0a2ed3a52
vctrhugoop <vhdestroy@hotmail.com>
2024-03-20 20:49:14 -0300

    🌈 style: position and add styles page 404

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -4,7 +4,7 @@ import { Header } from "../../components/Header";
 
 export function DefaultLayout() {
   return (
-    <div className="relative min-h-screen">
+    <div className="relative flex min-h-screen flex-col">
       <Header />
       <Outlet />
       <Footer />
--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -2,13 +2,13 @@ import image404 from "../assets/image404.svg";
 
 export function Page404() {
   return (
-    <div className="mx-auto flex max-w-6xl items-center justify-center">
-      <section className="mx-auto flex max-w-6xl items-center justify-center gap-8 text-center">
+    <div className="mx-auto flex max-w-6xl flex-1 items-center justify-center">
+      <section className="mb-32 flex items-center space-x-6">
         <img src={image404} className="hidden w-96 md:block" />
         <div>
-          <span className="text-8xl font-bold">404...</span>
-          <p className="text-xl text-muted-foreground">
-            Ops...Página não encontrada
+          <span className="text-9xl font-bold">404</span>
+          <p className="text-lg text-muted-foreground">
+            Ops...Página não encontrada.
           </p>
         </div>
       </section>
