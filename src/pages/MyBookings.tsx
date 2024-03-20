5835aa66588040c3cc286f10a1ea33b016f60b1a
vctrhugoop <vhdestroy@hotmail.com>
2024-03-20 09:34:09 -0300

    ✨ feat: setup card page my bookings

--- src/pages/MyBookings.tsx
+++ src/pages/MyBookings.tsx
@@ -7,11 +7,16 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { useBookingCar } from "@/hooks/useBooking";
-import { Calendar, Car, CurrencyDollar, User } from "@phosphor-icons/react";
+import { Calendar, Car, User } from "@phosphor-icons/react";
 import { format } from "date-fns";
+import { useEffect } from "react";
 
 export function MyBookings() {
   const { bookingsCars } = useBookingCar();
+
+  useEffect(() => {
+    document.title = "AlugaAí - Minhas Reservas";
+  }, []);
 
   return (
     <div className="mx-auto max-w-6xl ">
@@ -22,7 +27,7 @@ export function MyBookings() {
         </div>
       ) : (
         <section className="mx-auto max-w-6xl space-y-4 px-4 pb-48 pt-6">
-          <h1>Minhas Reservas</h1>
+          <h1 className="text-xl font-bold">Minhas Reservas</h1>
           {bookingsCars.map((bookingCar) => (
             <Card key={bookingCar.car.id} className="p-6">
               <CardHeader className="space-y-0 text-center">
@@ -32,45 +37,57 @@ export function MyBookings() {
                 </CardDescription>
               </CardHeader>
               <Separator className="my-4" />
-              <CardContent className="space-y-4">
+              <CardContent className="space-y-4 divide-y-[1px] lg:flex lg:space-x-4 lg:space-y-0 lg:divide-x-[1px] lg:divide-y-0">
                 <div className="space-y-2">
-                  <h3 className="flex font-semibold leading-none tracking-tight ">
+                  <h3 className="flex gap-1 font-semibold leading-none tracking-tight">
                     <Car />
                     Veículo
                   </h3>
                   <div>
-                    <p className="text-sm text-muted-foreground">
-                      Modelo: {bookingCar.car.name}
+                    <p className="text-sm font-semibold text-primary">
+                      Modelo:{" "}
+                      <span className="font-normal text-muted-foreground">
+                        {bookingCar.car.name}
+                      </span>
                     </p>
-                    <p className="text-sm text-muted-foreground">
-                      Categótia: {bookingCar.car.category}
+                    <p className="text-sm font-semibold text-primary">
+                      Categoria:{" "}
+                      <span className="font-normal text-muted-foreground">
+                        {bookingCar.car.category}
+                      </span>
                     </p>
+                    {/* <img
+                      src={bookingCar.car.imageURL}
+                      className="hidden w-48 lg:block"
+                    /> */}
                   </div>
-                  <img
-                    src={bookingCar.car.imageURL}
-                    className="hidden w-48 lg:block"
-                  />
                 </div>
-                <div className="space-y-2">
-                  <h3 className="flex font-semibold leading-none tracking-tight">
+                <div className="space-y-2 pt-4 lg:pl-4 lg:pt-0">
+                  <h3 className="flex gap-1 font-semibold leading-none tracking-tight">
                     <User />
                     Dados do condutor
                   </h3>
                   <div>
-                    <p className="text-sm text-muted-foreground">
-                      Nome: {bookingCar.formData.firstName}{" "}
-                      {bookingCar.formData.lastName}
+                    <p className="text-sm font-semibold text-primary">
+                      Nome:{" "}
+                      <span className="font-normal text-muted-foreground">
+                        {bookingCar.formData.firstName}{" "}
+                        {bookingCar.formData.lastName}
+                      </span>
                     </p>
-                    <p className="text-sm text-muted-foreground">
-                      E-mail: {bookingCar.formData.email}
+                    <p className="text-sm font-semibold text-primary">
+                      E-mail:{" "}
+                      <span className="font-normal text-muted-foreground">
+                        {bookingCar.formData.email}
+                      </span>
                     </p>
                   </div>
                 </div>
-                <div className="flex justify-between">
-                  <div className="space-y-2">
-                    <h3 className="flex font-semibold leading-none tracking-tight">
+                <div className="lg: flex justify-between gap-4 lg:flex-col lg:justify-normal">
+                  <div className="space-y-2 pt-4 lg:pl-4 lg:pt-0">
+                    <h3 className="flex gap-1 font-semibold leading-none tracking-tight">
                       <Calendar />
-                      Data de retirada
+                      Retirada
                     </h3>
                     <p className="text-sm text-muted-foreground">
                       {bookingCar.formData.date.from && (
@@ -80,10 +97,10 @@ export function MyBookings() {
                       )}
                     </p>
                   </div>
-                  <div className="space-y-2">
-                    <h3 className="flex font-semibold leading-none tracking-tight">
+                  <div className="space-y-2 pt-4 lg:pl-4 lg:pt-0">
+                    <h3 className="flex gap-1 font-semibold leading-none tracking-tight">
                       <Calendar />
-                      Data de devolução
+                      Devolução
                     </h3>
                     <p className="text-sm text-muted-foreground">
                       {bookingCar.formData.date.to && (
@@ -94,14 +111,46 @@ export function MyBookings() {
                     </p>
                   </div>
                 </div>
-                <div>
-                  <h3 className="flex font-semibold leading-none tracking-tight">
-                    <CurrencyDollar />
-                    Valor
-                  </h3>
-                  <p className="text-sm text-muted-foreground">
-                    {bookingCar.car.price}
-                  </p>
+                <div className="space-y-4 pt-4 lg:flex-1 lg:pl-4 lg:pt-0">
+                  <div>
+                    <h3 className="font-semibold leading-none tracking-tight">
+                      Diária
+                    </h3>
+                    <div className="flex items-end justify-between">
+                      <span className="text-sm text-muted-foreground">
+                        5x {bookingCar.car.price}
+                      </span>
+                      <span>{bookingCar.car.price}</span>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold leading-none tracking-tight ">
+                      Proteção {bookingCar.formData.protection}
+                    </h3>
+                    <div className="flex items-end justify-between">
+                      <span className="text-sm text-muted-foreground">
+                        5x 9,00
+                      </span>
+                      <span>9,00</span>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold leading-none tracking-tight">
+                      Taxa administrativa
+                    </h3>
+                    <div className="flex items-end justify-between">
+                      <span className="text-sm text-muted-foreground">
+                        Taxa Fixa de 12%
+                      </span>
+                      <span>9,00</span>
+                    </div>
+                  </div>
+                  <div className="flex items-end justify-between">
+                    <h3 className="flex gap-1 text-xl font-semibold leading-none tracking-tight">
+                      Valor Total{" "}
+                    </h3>
+                    <p>{bookingCar.car.price}</p>
+                  </div>
                 </div>
               </CardContent>
             </Card>
