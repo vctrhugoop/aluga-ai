249034956a404bf4fa23907db1e045a2b951cfa6
vctrhugoop <vhdestroy@hotmail.com>
2024-03-21 17:18:03 -0300

    🐞 fix: add wfull

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,8 @@ import { CarsList } from "../components/CarsList";
 
 export function Home() {
   return (
-    <div className="mx-auto max-w-6xl">
-      <section className="mx-auto my-9 flex max-w-6xl items-center justify-between px-4 py-6">
+    <div className="mx-auto w-full max-w-6xl">
+      <section className="mx-auto my-9 flex  max-w-6xl items-center justify-between px-4 py-6">
         <div className="space-y-2">
           <h1 className="text-2xl font-bold  lg:text-4xl">
             Desbrave o Caminho da Liberdade
